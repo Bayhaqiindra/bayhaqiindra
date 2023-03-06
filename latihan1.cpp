39a16eed9a831d3bd63ed5866fb283828b02a4c7
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-03-06 12:23:27 +0700

    latihan2

---
+++ latihan1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Begin'
+    // Numeric Luas,p,L
+    double Luas,p,L;
+    // display 'Masukin Panjangnya = '
+    cout << "Masukin Panjangnya = ";
+    // Accept p
+    cin >> p;
+    // display 'Masukin Lebarnya = '
+    cout << "Masukin Lebarnya = ";
+    // Accept L
+    cin >> L;
+    // compute luas = p*l
+    cout << "compute luas = p*l";
+    Luas = p*L;
+    // display "luasnya = "+luas
+    cout << "luasnya = " <<Luas;
+    // end
+    
+
+
+}
